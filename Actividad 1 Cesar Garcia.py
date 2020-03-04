bec582f04cf2fbac883c8d752933350df541b582
Cesargg13 <61669524+Cesargg13@users.noreply.github.com>
2020-03-04 00:06:08 -0600

    Python 01
    code

---
+++ Actividad 1 Cesar Garcia.py
@@ -0,0 +1,19 @@
+FIRST_NAME="Cesar "
+LAST_NAME="Garcia Garcia"
+AGE="18"
+DRIVERS_LIC=True
+HEIGHT="1.7m"
+number = '7712221746'
+ine =True
+
+print('{:20}{} {}'.format('Name: ', FIRST_NAME,LAST_NAME))
+
+print('{:20}{}'.format('Age: ', AGE))
+
+print('{:20}{}'.format('Height: ', HEIGHT))
+
+print('{:25}{}'.format("Has Driver's license?: ",DRIVERS_LIC))
+
+print('{:20}{}'.format('Phone Number: ', number))
+
+print('{:20}{}'.format('Has ine?: ', ine))
